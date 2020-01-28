2aed99adab69c1d943a4071de0e3d5c08e3e439a
daniel.casado <jdanielca@gmail.com>
2020-01-28 15:25:37 -0500

    Adds example Godot project (with some JSON)

---
+++ JSONLoader.gd
@@ -0,0 +1,17 @@
+extends Node
+
+static func LoadJSON(filename):
+	var dataFile = File.new()
+	if (dataFile.open("res://" + filename, File.READ) != OK):
+		print("Can't find json file")
+		return false
+	var dataText = dataFile.get_as_text()
+	dataFile.close()
+	var parsedJSON = JSON.parse(dataText)
+	if parsedJSON.error != OK:
+		print("Couldn't parse json file")
+		return false
+	if typeof(parsedJSON.result) != TYPE_DICTIONARY:
+		print("The JSON root object is not a map")
+		return false
+	return parsedJSON.result
