8ef640b864e7262f12f724e4f0a403af87f37766
Sen <laisenbbq@gmail.com>
2020-03-04 19:37:45 -0500

    spite flash red function

--- Enemy.gd
+++ Enemy.gd
@@ -5,9 +5,23 @@ const MOVE_SPEED = 50
 onready var raycast = $RayCast2D
 
 var player = null
+var redState = false
 
 func _ready():
 	add_to_group("enemies")
+	$Timer.start();
+
+# Make the Sprite flash to red color based on Timer
+func FlashRed(var duration):
+	if $Timer.time_left == 0:
+		redState = !redState
+		$Timer.set_wait_time(duration)
+		$Timer.start();
+	if redState:
+		$AnimatedSprite.set_modulate(Color(1, 0, 0))		
+	else:
+		$AnimatedSprite.set_modulate(Color(1, 1, 1))		
+	
 
 func _physics_process(delta):
 	if player == null:
@@ -17,6 +31,8 @@ func _physics_process(delta):
 	vec_to_player = vec_to_player.normalized()
 	global_rotation = atan2(vec_to_player.y, vec_to_player.x)
 	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+
+	FlashRed(0.5)
 	
 	if raycast.is_colliding():
 		var coll = raycast.get_collider()
