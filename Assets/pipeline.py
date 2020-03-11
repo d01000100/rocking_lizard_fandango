06b9266321f997a85744f3438c5c24e7109f5358
daniel.casado <jdanielca@gmail.com>
2020-03-11 16:37:12 -0400

    Adds comments to python script to convert images

--- Assets/pipeline.py
+++ Assets/pipeline.py
@@ -1,6 +1,8 @@
 import os
 from PIL import Image
 
+# Quantize an image in `file` filepath and
+# stores it in a copy with `_qnt` appended
 def qntizeImage(file):
     filename = file[:-4]
     # check if qnt already exists
@@ -13,6 +15,8 @@ def qntizeImage(file):
     newImgQNT = img.quantize()
     newImgQNT.save(filename+"_qnt.png",'PNG')
 
+# Optimizes an image in `file` filepath without
+# loss and stores a copy with `_opti` appended
 def optiImage(file):
     filename = file[:-4]
     # check if opti already exists
@@ -23,6 +27,8 @@ def optiImage(file):
     if filename.endswith("_opti"): return
     os.system("optipng.exe -quiet -out " + filename + "_opti.png " + file)
 
+# Optimizes an image in `file` to use as a texture
+# and stores a copy with `.dds` extension
 def ddsImage(file):
     filename = file[:-4]
     # check if dds already exists
@@ -33,12 +39,16 @@ def ddsImage(file):
     if filename.endswith("_opti"): return
     os.system("texconv.exe " + file)
 
+# Checks if `filepath` file already exists
 def fileAlreadyExists(filepath):
     for file in os.listdir("."):
         if file == filepath:
             return True
     return False
 
+# Quintizes, optimizes losslessly and optimizes
+# for textures all of the `.png` images in the 
+# current directory
 print("starting to convert images!...")
 for file in os.listdir("."):
     if file.endswith(".png"):
